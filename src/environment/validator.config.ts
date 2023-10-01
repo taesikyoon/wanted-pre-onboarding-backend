722e4a2d401a548c57a250849e60c4f107e7585b
taesik <taesikyoon97@naver.com>
2023-10-01 11:20:40 +0900

    초기 세팅 - 환경변수, 데이터베이스(TypeORM)

---
+++ src/environment/validator.config.ts
@@ -0,0 +1,20 @@
+import { plainToClass } from 'class-transformer';
+import { validateSync } from 'class-validator';
+import { ClassConstructor } from 'class-transformer/types/interfaces';
+
+export default function ValidateConfig<T extends object>(
+  config: Record<string, unknown>,
+  envClass: ClassConstructor<T>,
+) {
+  const validatedConfig = plainToClass(envClass, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+}
