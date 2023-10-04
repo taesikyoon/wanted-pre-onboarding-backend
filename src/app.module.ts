2cb5e47931a16b46d9e8ca7761213dab64920b42
taesik <taesikyoon@10k1m.com>
2023-10-04 17:37:22 +0900

    초기 생성 작업

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvironmentModule } from './environment/environment.module';
+import { JobPostingModule } from './job-posting/job-posting.module';
+import { DatabaseModule } from './database/database.module';
+import { CompanyModule } from './company/company.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [EnvironmentModule],
+  imports: [EnvironmentModule, JobPostingModule, DatabaseModule, CompanyModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
