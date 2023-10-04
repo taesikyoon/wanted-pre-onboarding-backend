8522db50fa16fd50d302063f8a3df9f810062232
taesik <taesikyoon97@naver.com>
2023-10-04 17:35:58 +0900

    초기 생성 작업 - 환경 변수

--- src/environment/environment.module.ts
+++ src/environment/environment.module.ts
@@ -6,6 +6,7 @@ import databaseConfig from './values/database.config';
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       envFilePath: `.${process.env.NODE_ENV}.env`,
       load: [appConfig, databaseConfig],
     }),
