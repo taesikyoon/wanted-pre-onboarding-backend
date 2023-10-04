c664baab3b767062488f6a06d78508bb9acda564
taesik <taesikyoon97@naver.com>
2023-10-04 18:57:37 +0900

    feat: 유효성 검사를 위한 글로벌 파이프 등록

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AppConfig } from './environment/values/app.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const appConfig: AppConfig = configService.get('APP');
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(appConfig.APP_PORT);
 }
 bootstrap();
