816463f4660c4992a85d21e557a9ace65a9dcc9a
taesik <taesikyoon97@naver.com>
2023-10-05 13:02:28 +0900

    chore: 채용공고 목록 가져오기에 DTO 수정 파라미터로 받는 페이지네이션 속성들이 문자열로 인식되는 부분을 숫자로 인식할 수 있도록 변경

--- src/job-posting/dto/find-many-job-posting.dto.ts
+++ src/job-posting/dto/find-many-job-posting.dto.ts
@@ -1,10 +1,13 @@
-import { IsIn, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsIn, IsInt, IsNumber } from 'class-validator';
 
 export class FindManyJobPostingDto {
-  @IsNumber()
+  @IsInt()
+  @Type(() => Number)
   page = 1;
 
-  @IsNumber()
+  @IsInt()
+  @Type(() => Number)
   items = 10;
 
   @IsIn(['DESC', 'ASC'])
