8a83a61549a9d5ff2d67bd110907daeb927860d1
taesik <taesikyoon97@naver.com>
2023-10-06 00:07:49 +0900

    feat: 채용공고 목록 가져오기 기능 업데이트 요구 사항에 맞게 설계되지 않은 부분을 확인하여 요구사항에 맞게 채용공고 목록 가져오도록 변경, 쿼리빌더를 이용하여 데이터로 가져오는 방법으로 변경

--- src/job-posting/models/jop-postring.dao.ts
+++ src/job-posting/models/jop-postring.dao.ts
@@ -1,20 +1,29 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { JobPosting } from './job-posting.entity';
 import { Repository } from 'typeorm';
-import { CreateJobPostingDto } from '../dto/create-job-posting.dto';
-import { FindManyJobPostingDto } from '../dto/find-many-job-posting.dto';
+import { CreateJobPostingDTO } from '../dto/request/create-job-posting.dto';
+import { FindManyJobPostingDTO } from '../dto/request/find-many-job-posting.dto';
+import { JobPostingMapper } from '../dto/response/jop-posting-list.mapper';
 
 export class JobPostingDAO {
   constructor(@InjectRepository(JobPosting) private readonly jobPostingRepository: Repository<JobPosting>) {}
 
-  async create(jobPosting: CreateJobPostingDto) {
+  async create(jobPosting: CreateJobPostingDTO) {
     await this.jobPostingRepository.insert(jobPosting);
   }
 
-  async findMany(findManyOptions: FindManyJobPostingDto) {
+  async findMany(findManyOptions: FindManyJobPostingDTO) {
     const skip = (findManyOptions.page - 1) * findManyOptions.items;
     const take = findManyOptions.items;
-    // 정렬의 기준은 create_at으로 했으나 seed로 생성된 데이터들로 인해 id를 기준으로 정렬하도록 변경
-    return await this.jobPostingRepository.find({ skip, take, order: { id: findManyOptions.sort }, cache: true });
+    const jopPostingList = await this.jobPostingRepository
+      .createQueryBuilder('JB')
+      .select(['JB.id', 'JB.position', 'JB.technicalStack', 'C.name', 'C.country', 'C.region'])
+      .leftJoin('JB.company', 'C', 'JB.companyId = C.id')
+      .orderBy('JB.createdAt', findManyOptions.sort)
+      .offset(skip)
+      .limit(take)
+      .getMany();
+
+    return jopPostingList.map(item => new JobPostingMapper(item));
   }
 }
